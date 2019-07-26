c327f24e02a21702d4ee38c1bbcf028e12a63eef
moparisthebest <admin@moparisthebest.com>
2019-07-26 00:56:37 -0400

    Make FunctionSupplier a FunctionalInterface

--- common/src/main/java/com/moparisthebest/jdbc/cache/FunctionSupplier.java
+++ common/src/main/java/com/moparisthebest/jdbc/cache/FunctionSupplier.java
@@ -3,9 +3,12 @@ package com.moparisthebest.jdbc.cache;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+@FunctionalInterface
 public interface FunctionSupplier<T, F> extends Supplier<T>, Function<F, T> {
 
-    T get(F f);
+    default T get(final F f) {
+        return get();
+    }
 
     @Override
     default T apply(final F f) {
